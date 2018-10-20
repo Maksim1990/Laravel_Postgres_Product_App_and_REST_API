3b6f82b55ad9464888fe18af2ef0ca0b2d561aca
Your Name <you@example.com>
2018-10-20 17:06:06 +0300

    - Changed request validation when product item is updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -104,7 +104,7 @@ class ProductController extends Controller
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductCreateRequest $request, $id)
     {
 
 
